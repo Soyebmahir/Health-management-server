3969f55e1d3c690ac801c35552abfd99f22f1e0f
Soyeb <tssoyeb@gmail.com>
2022-05-28 17:43:00 +0600

    fixing

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cors());
 app.use(express.json()); 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t36og.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
